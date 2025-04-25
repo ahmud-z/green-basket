a92277ffd2766709ed5e9241238294879006d644
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2025-04-25 22:44:55 +0600

    feat: favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,6 +6,6 @@
     @vite('resources/css/app.css')
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
